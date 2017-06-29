c990c54aac3189d4eb1572a66a60f7c4ca3d9b72
VARDALAS VINCENT p1611561 <p1611561@univ-lyon1.fr>
2017-06-29 17:00:08 +0200

    remplissage classes

--- SolidExercices/Multiplication.cs
+++ SolidExercices/Multiplication.cs
@@ -8,5 +8,19 @@ namespace SolidExercices
 {
     class Multiplication
     {
+        public decimal Calculate(string operation)
+        {
+            decimal result = 0;
+            if (operation.Contains("*"))
+            {
+                string[] numbers = operation.Split('*');
+                result = Convert.ToDecimal(numbers[0]);
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    result *= Convert.ToDecimal(numbers[i]);
+                }
+            }
+            return result;
+        }
     }
 }
