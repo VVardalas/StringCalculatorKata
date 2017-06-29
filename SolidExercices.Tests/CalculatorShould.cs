703f713956074582e126808e5b3a5c209b88bb5a
VARDALAS VINCENT p1611561 <p1611561@univ-lyon1.fr>
2017-06-29 14:44:31 +0200

    tests 4 opérations

--- SolidExercices.Tests/CalculatorShould.cs
+++ SolidExercices.Tests/CalculatorShould.cs
@@ -12,5 +12,29 @@ namespace SolidExercices.Tests
             var result = calculator.Calculate("1+2,3");
             Check.That(result).IsEqualTo(3.3m);
         }
+
+        [Test]
+        public void CalculateASoustraction()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("2,3-1");
+            Check.That(result).IsEqualTo(1.3m);
+        }
+
+        [Test]
+        public void CalculateAMultiplication()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("5*10");
+            Check.That(result).IsEqualTo(50m);
+        }
+
+        [Test]
+        public void CalculateADivision()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("14/2");
+            Check.That(result).IsEqualTo(7m);
+        }
     }
 }
